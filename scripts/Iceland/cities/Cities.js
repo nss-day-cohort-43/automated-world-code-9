c58ac59d11c7cc9b46c93b0e387b9d8ea29f0a2a
Ember Parr <ember-devops@gmail.com>
2020-08-24 12:12:58 -0500

    did html rep for cities

---
+++ scripts/Iceland/cities/Cities.js
@@ -0,0 +1,11 @@
+export const City = (cityObj) => {
+    return `
+    <div class="city-card">
+        <div class="card-image">
+            <img class="city-image" src="/images/${cityObj.image}" alt="${cityObj.name} Image">
+        </div>
+        <p> City Name: ${cityObj.name} </p>
+        <p> Population: ${cityObj.name} </p>
+    </div>
+    `
+}
--- scripts/Iceland/main.js
+++ scripts/Iceland/main.js
@@ -1,7 +1,13 @@
 import { icelandicCities } from './cities/CityDataProvider.js';
+import { cityList } from './cities/CityList.js'
+
+
 import { icelandicLandmarks } from './landmarks/LandmarkDataProvider.js';
 import { icelandicLeaders } from './leaders/LeaderDataProvider.js';
 
 icelandicCities();
 icelandicLandmarks();
 icelandicLeaders();
+
+
+cityList();
