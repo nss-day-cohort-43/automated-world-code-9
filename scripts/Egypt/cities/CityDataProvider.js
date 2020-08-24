1476f3a77623db4f6b58e2fc2706ec78308bf93b
Dani Crosby <dani.crosby1111@gmail.com>
2020-08-24 11:52:16 -0500

    updated dataproviders

--- scripts/Egypt/cities/CityDataProvider.js
+++ scripts/Egypt/cities/CityDataProvider.js
@@ -2,12 +2,18 @@
 export const egyptCities = () => [
     {
         cities: "Cairo"
+        longitude: 30.7964,
+        latitude: 26.8371
     },
     {
         cities: "Giza"
+        longitude: 31.131302,
+        latitude: 29.976480
     },
     {   
         cities: "Alexandria"
+        longitude: 29.924526,
+        latitude: 31.205753
     }
 
 ]
--- scripts/Egypt/leaders/LeaderDataProvider.js
+++ scripts/Egypt/leaders/LeaderDataProvider.js
@@ -1,12 +1,19 @@
 export const egyptLeaders = () => [
     {
-        leaders: "Cairo"
+        leaders: "Hatshepsut",
+        dynasty: 18,
+        years reigned: 15
     },
     {
-        leaders: "Giza"
+        leaders: "Ramesses II"
+        dynasty: 19,
+        years reigned: 16
+        
     },
     {   
-        leaders: "Alexandria"
+        leaders: "Khufu"
+        dynasty: 4,
+        years reigned: 26
     }
 
 ]
