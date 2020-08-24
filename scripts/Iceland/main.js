a197b417d53a7bdefd5cd9ffa16c00a18550d481
Ember Parr <ember-devops@gmail.com>
2020-08-24 14:14:04 -0500

    cleaned up main js to only include necessary info

--- scripts/Iceland/main.js
+++ scripts/Iceland/main.js
@@ -1,17 +1,6 @@
-import { icelandicCities } from './cities/CityDataProvider.js';
 import { cityList } from './cities/CityList.js'
-
-
-import { icelandicLandmarks } from './landmarks/LandmarkDataProvider.js';
 import { landmarkList } from './landmarks/LandmarkList.js'
-
-import { icelandicLeaders } from './leaders/LeaderDataProvider.js';
 import { leaderList } from './leaders/LeaderList.js'
-
-
-icelandicCities();
-icelandicLandmarks();
-icelandicLeaders();
 
 
 cityList();
